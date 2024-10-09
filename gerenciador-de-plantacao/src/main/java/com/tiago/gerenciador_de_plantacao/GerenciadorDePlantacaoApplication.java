e7c3a1250cdfb083444d6aab54cad0132394984d
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-10-09 09:54:57 -0300

    criação com o initializer

---
+++ gerenciador-de-plantacao/src/main/java/com/tiago/gerenciador_de_plantacao/GerenciadorDePlantacaoApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.gerenciador_de_plantacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePlantacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePlantacaoApplication.class, args);
+	}
+
+}
---
+++ gerenciador-de-plantacao/src/test/java/com/tiago/gerenciador_de_plantacao/GerenciadorDePlantacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.gerenciador_de_plantacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePlantacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
